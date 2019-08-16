608cc2727343bfcc8195ce82cf7561fe752bc2e6
Amee <ameecova@yahoo.com>
2019-08-16 12:13:11 -0700

    created helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,23 +13,18 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-  get '/login' do
-    erb :login
-  end
 
-  post '/login' do
-    @user = User.create(params[:user])
-    session[:user_id] = @user.id
-  end
-
-  get '/logout' do
-    session.destroy
-    redirect '/'
-  end
 
   helpers do
     def current_user
-      User.find_by(:id => session[:user_id])
+      #minimalization
+      @current_user =|| User.find_by(:id => session[:user_id])
+    end
+
+    #returns a boolean
+    def logged_in?
+      #current_user will return nil or the entire user object( we want true or false)
+      !!current_user
     end
   end
 
