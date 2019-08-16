f588003ac199eceaf3f942d7d0e5176ee08e2db7
Amee <ameecova@yahoo.com>
2019-08-16 16:18:41 -0700

    created redirect if not logged in helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,13 +18,19 @@ class ApplicationController < Sinatra::Base
   helpers do
     def current_user
       #minimalization
-      @current_user =|| User.find_by(:id => session[:user_id])
+      @current_user ||= User.find_by(:id => session[:user_id])
     end
 
     #returns a boolean
     def logged_in?
       #current_user will return nil or the entire user object( we want true or false)
       !!current_user
+    end
+
+    def redirect_if_not_logged_in?
+      if !logged_in?
+        redirect '/'
+      end
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,25 @@ class UsersController < ApplicationController
 
   # PATCH: /users/5
   patch "/users/:id" do
-    redirect "/users/:id"
+    #we are getting id from url ^
+    @user = User.find_by(id: params[:id])
+    erb :show
+  end
+
+  #what routes do we need for signup?
+  get '/signup' do
+    #render my sign form
+    #that form would point to a post '/users' method
+  end
+
+  post '/users' do
+    #only create user is they provide username, email, and password
+    #if all data is present
+      #add key/value pair
+      #redirect to user profile
+    #else
+      #try again
+    redirect_if_not_logged_in?
   end
 
   # DELETE: /users/5/delete
